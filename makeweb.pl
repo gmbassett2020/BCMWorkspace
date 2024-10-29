5d90f852206d4894dda680d4090951f9eb602804
Gene Bassett <gmbassett@gmail.com>
2024-10-29 11:26:12 -0700

    NFL 2024 week 9 predictions

--- makeweb.pl
+++ makeweb.pl
@@ -23,8 +23,8 @@ my $html_base = "./html";
 #FIXME
 my $year = "2024";
 my $league = "nfl";
-my $week = "week08"; # this is the current week, the result week is one less
-my $today = "22 Oct 2024";
+my $week = "week09"; # this is the current week, the result week is one less
+my $today = "29 Oct 2024";
 #my $year = "2023";
 #my $league = "col";
 #my $week = "wke3"; # this is the current week, the result week is one less, wkb#,wk##,wke#
