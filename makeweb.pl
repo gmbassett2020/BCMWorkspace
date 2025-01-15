caa1d961305a6919b20ffed5bdbfae77e3bb3b27
Gene Bassett <gmbassett@gmail.com>
2025-01-14 19:26:07 -0800

    NFL 2024 Division predictions

--- makeweb.pl
+++ makeweb.pl
@@ -23,8 +23,8 @@ my $html_base = "./html";
 #FIXME
 my $year = "2024";
 my $league = "nfl";
-my $week = "weeke1"; # this is the current week, the result week is one less
-my $today = "9 Jan 2025";
+my $week = "weeke2"; # this is the current week, the result week is one less
+my $today = "14 Jan 2025";
 #my $year = "2023";
 #my $league = "col";
 #my $week = "wke3"; # this is the current week, the result week is one less, wkb#,wk##,wke#
