ab9b56f615ae30c71c5e95f9ea76e066286bcfeb
Gene Bassett <gmbassett@gmail.com>
2025-01-27 11:31:18 -0800

    NFL 2024 season superbowl prediction

--- makeweb.pl
+++ makeweb.pl
@@ -23,8 +23,8 @@ my $html_base = "./html";
 #FIXME
 my $year = "2024";
 my $league = "nfl";
-my $week = "weeke3"; # this is the current week, the result week is one less
-my $today = "23 Jan 2025";
+my $week = "weeke4"; # this is the current week, the result week is one less
+my $today = "27 Jan 2025";
 #my $year = "2023";
 #my $league = "col";
 #my $week = "wke3"; # this is the current week, the result week is one less, wkb#,wk##,wke#
