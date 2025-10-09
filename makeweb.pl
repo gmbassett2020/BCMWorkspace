49c06e72baf4ee2381e3bbf20ae0ef6a9972042f
Gene Bassett <bfm@bassettfootball.net>
2025-10-08 18:15:48 -0600

    NFL 2025 week06

--- makeweb.pl
+++ makeweb.pl
@@ -23,8 +23,8 @@ my $html_base = "./html";
 #FIXME
 my $year = "2025";
 my $league = "nfl";
-my $week = "week05"; # this is the current week, the result week is one less
-my $today = "30 Sep 2025";
+my $week = "week06"; # this is the current week, the result week is one less
+my $today = "10 Oct 2025";
 #my $year = "2023";
 #my $league = "col";
 #my $week = "wke3"; # this is the current week, the result week is one less, wkb#,wk##,wke#
