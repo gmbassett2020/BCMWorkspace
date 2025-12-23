62d72d17ced6b8fb69ae5500e822e12d3be0ce99
Gene Bassett <bfm@bassettfootball.net>
2025-12-23 12:28:48 -0700

    2025 NFL week 16 & 17

--- makeweb.pl
+++ makeweb.pl
@@ -23,8 +23,8 @@ my $html_base = "./html";
 #FIXME
 my $year = "2025";
 my $league = "nfl";
-my $week = "week16"; # this is the current week, the result week is one less
-my $today = "16 Dec 2025";
+my $week = "week17"; # this is the current week, the result week is one less
+my $today = "23 Dec 2025";
 #my $year = "2023";
 #my $league = "col";
 #my $week = "wke3"; # this is the current week, the result week is one less, wkb#,wk##,wke#
